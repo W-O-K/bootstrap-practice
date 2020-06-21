2a450da32a943ea6bf430dfbf1032babe74cb5ee
W-O-K <wojiakor@gmail.com>
2020-06-21 01:50:42 +0100

    added sticky nav

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // Sticky navigation
 
-let navbar = $('.navbar');
+
 
 $(window).scroll(function(){
     let oTop = $(".section-2").offset().top - window.innerHeight;
     if($(window).scrollTop()>oTop){
-        navbar.addClass('sticky');
+        $('.navbar').addClass('sticky');
     }else{
-        navbar.removeClass('sticky');
+        $('.navbar').removeClass('sticky');
 }
 });
 
